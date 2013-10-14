6ff2162557192ce7c01ad8b314508343e8e02891
tobischuschnig <tobi@Tobiass-MacBook-Pro.local>
2013-10-14 20:03:56 +0200

    PrimeSearcher verbessert

--- src/java/primesearcher/PrimeSearcher.java
+++ src/java/primesearcher/PrimeSearcher.java
@@ -24,4 +24,34 @@ public class PrimeSearcher extends Thread{
         primes.add(2);
         
     }
+    
+    
+    @Override
+    public void run() {
+        
+    }
+    
+    /**
+     * Gibt die letzte berechnete Primzahl zurueck
+     * @return letzte Primzahl
+     */
+    public int getPrime() {
+        return primes.get(primes.size() - 1);
+    }
+
+    /**
+     * Gibt die Zeit zurueck bei der zuletzt eine Primzahl gefunden wurde
+     * @return Letzte Zeit
+     */
+    public Date getLastTime() {
+        return ltime;
+    }
+    
+    /**
+     * Gibt die Beginn Zeit zurueck
+     * @return Beginn Zeit
+     */
+    public Date getBeginnTime() {
+        return btime;
+    }
 }
